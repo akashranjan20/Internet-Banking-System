7385ee4e6e2824e99ffa51c3538994c95812e8f2
Akash <60097953+akashranjan20@users.noreply.github.com>
2022-04-29 13:38:02 +0530

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,21 +1,55 @@
-create database "Banking-System"
-use "Banking-System"
+--For Administrator part, we have to make two tables. One for Branch Manager Details and another one for Branch information.
+--Table names are Branch_Manager_Details and Branch_Info.
 
-create table UserData (
-	AccountNumber numeric(9) Primary Key,
-	Title varchar(4) Not Null,
+--Database for Branch Manager
+
+create database "Branch_Manager_Details"
+use "Branch_Manager_Details"
+
+create table Branch_Manager_Data (
+	BMID numeric(9) Primary Key,
 	Name varchar(25) Not Null,
-	TotalBalance float(15) Not Null,
-	Password varchar(22) Not Null,
-	LastLoginDetails smallDatetime
+	Address varchar(25) Not Null,
+	Phone_Number numeric(10) Not Null
 )
 
-create table Passbook (
-	AccountNumber numeric(9) Not Null,
-	Amount float(15) Not Null,
-	TimeOfTransaction smalldatetime,
-	Mode varchar(25) Not Null
+INSERT INTO Branch_Manager_Data (BMID, Name, Address, Phone_Number)
+VALUES ('1', 'Aarav Singh', 'Delhi', '9685741230');
+INSERT INTO Branch_Manager_Data (BMID, Name, Address, Phone_Number)
+VALUES ('2', 'Aashlesha Kamlakar Patil', 'Noida', '9999999999');
+INSERT INTO Branch_Manager_Data (BMID, Name, Address, Phone_Number)
+VALUES ('3', 'Aditi Sanjeevan Kande', 'Gurgaon', '9868686868');
+INSERT INTO Branch_Manager_Data (BMID, Name, Address, Phone_Number)
+VALUES ('4', 'Akash Ranjan', 'Faridabad', '9898989898');
+INSERT INTO Branch_Manager_Data (BMID, Name, Address, Phone_Number)
+VALUES ('5', 'Aleti Varshini', 'Hyderabad', '9696969696');
+INSERT INTO Branch_Manager_Data (BMID, Name, Address, Phone_Number)
+VALUES ('6', 'Areeb Aasan Md', 'Mumbai', '9595959595');
+INSERT INTO Branch_Manager_Data (BMID, Name, Address, Phone_Number)
+VALUES ('7', 'Bedidha Praneeth Sai', 'Bangalore', '9797979797');
+INSERT INTO Branch_Manager_Data (BMID, Name, Address, Phone_Number)
+VALUES ('8', 'Bindu Madhavi Gorle', 'Chennai', '9256347800');
+INSERT INTO Branch_Manager_Data (BMID, Name, Address, Phone_Number)
+VALUES ('9', 'Sai Sindhu ', 'Delhi', '9685741230');
+INSERT INTO Branch_Manager_Data (BMID, Name, Address, Phone_Number)
+VALUES ('1', 'Aarav Singh', 'Delhi', '9685741230');
+INSERT INTO Branch_Manager_Data (BMID, Name, Address, Phone_Number)
+VALUES ('1', 'Aarav Singh', 'Delhi', '9685741230');
+INSERT INTO Branch_Manager_Data (BMID, Name, Address, Phone_Number)
+VALUES ('1', 'Aarav Singh', 'Delhi', '9685741230');
+
+select * from Branch_Manager_Data
+
+--Database for Branch Info
+
+create database "Branch_Information"
+use "Branch_Information"
+
+create table Branch_Info (
+	Branch_ID numeric(9) Not Null,
+	Total_Balance numeric(9) Not Null,
+	Branch_Address varchar(25) Not Null
 )
 
-select * from Passbook
-select * from UserData
+select * from Branch_Info
+
